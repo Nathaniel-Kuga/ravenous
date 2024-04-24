ca61d66ec7fda55ca24ae840a202c5befd001016
Nathaniel Kuga <natelkuga@protonmail.com>
2024-04-24 09:47:31 -0700

    NK: Setting up Business component.

---
+++ src/Business.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// const Business =
+// {
+//     image:
+// };
+
+function Business() {
+    return (
+        <div className="business">
+            <img src="restaurant-image.jpg" alt="Restaurant" />
+            <h2>Restaurant Name</h2>
+            <p>123 Main St</p>
+            <p>City, State 12345</p>
+            <p>Category: Italian</p>
+            <p>Rating: 4.5 stars</p>
+            <p>Reviews: 123</p>
+        </div>
+    );
+}
+
+export default Business;
