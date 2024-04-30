be755a1671d156ae449e1f511a9e366419913442
Nathaniel Kuga <natelkuga@protonmail.com>
2024-04-30 08:51:39 -0700

    NK: building react business component.

--- src/Business.js
+++ src/Business.js
@@ -1,23 +1,47 @@
 import React from 'react';
 
-// const Business =
-// {
-//     image:
-// };
-
-function Business(business) {
+const Business = ({ business }) => {
     return (
         <div className="business">
-            <img src={business.image} alt="Restaurant" />
+            <img src={business.image} alt={business.name} />
             <h2>{business.name}</h2>
             <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.zipcode}</p>
-            <p>{business.category}</p>
-            <p>{business.rating}</p>
-            <p>{business.reviewCount}</p>
+            <p>
+                {business.city}, {business.state} {business.zipcode}
+            </p>
+            <p>Category: {business.category}</p>
+            <p>Rating: {business.rating}</p>
+            <p>Reviews: {business.reviewCount}</p>
         </div>
     );
-}
+};
 
 export default Business;
+
+// const BusinessObj =
+// {
+//     image: '',
+//     name: '',
+//     address: '',
+//     city: '',
+//     zipcode: '',
+//     category: '',
+//     rating: '',
+//     reviewCount: ''
+// };
+
+// function Business(business) {
+//     return (
+//         <div className="business">
+//             <img src={business.image} alt="Restaurant" />
+//             <h2>{business.name}</h2>
+//             <p>{business.address}</p>
+//             <p>{business.city}</p>
+//             <p>{business.zipcode}</p>
+//             <p>{business.category}</p>
+//             <p>{business.rating}</p>
+//             <p>{business.reviewCount}</p>
+//         </div>
+//     );
+// }
+// Business.js
